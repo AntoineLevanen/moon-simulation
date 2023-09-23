11ac054216413dd1538d9e695cb1225d0f6dcd81
Antoine <92716096+AntoineL56@users.noreply.github.com>
2023-09-23 15:54:32 +0200

    moon interacting with planet

--- main.cpp
+++ main.cpp
@@ -4,7 +4,28 @@ using namespace std;
 
 #include <SFML/Graphics.hpp>
 
-class Particle
+class Planet
+{
+public:
+    sf::Vector2f position;
+    float gravity_strengh;
+    sf::CircleShape circle;
+
+    Planet(sf::Vector2f pos, float gravity, int radius):position(pos), gravity_strengh(gravity){
+        circle.setRadius(radius);
+        circle.setOrigin(sf::Vector2f(radius, radius));
+        circle.setFillColor(sf::Color(125, 190, 230));
+        circle.setPosition(position);
+    }
+
+    void render(sf::RenderWindow &win){
+        win.draw(circle);
+    }
+
+};
+
+
+class Moon
 {
 public:
     sf::CircleShape circle;
@@ -12,83 +33,77 @@ public:
     sf::Vector2f velocity;
     sf::Vector2f acceleration;
     float mass;
-    float radius;
 
-    Particle(double r, sf::Vector2f pos, sf::Vector2f vel, float mass):radius(r), position(pos), velocity(vel), mass(mass){
+    Moon(sf::Vector2f pos, sf::Vector2f vel, int radius):position(pos), velocity(vel){
         circle.setRadius(radius);
         circle.setOrigin(sf::Vector2f(radius, radius));
+        circle.setFillColor(sf::Color(140, 140, 140));
         acceleration = sf::Vector2f(0, 0);
     }
 
-    void update(float &dt){
+    void update(Planet &planet, float &dt){
+        
+        // compute distance between planet and moon
+        float dis_x = planet.position.x - position.x;
+        float dis_y = planet.position.y - position.y;
 
-        velocity.x = acceleration.x * dt;
-        velocity.y = acceleration.y * dt;
+        float direction = sqrt(dis_x * dis_x + dis_y * dis_y);
 
-        position.x += velocity.x * dt * 60;
-        position.y += velocity.y * dt * 60;
-        circle.setPosition(position);
+        float invert_direction = 1.f / direction;
+
+        sf::Vector2f normalize_direction = sf::Vector2f(dis_x * invert_direction, dis_y * invert_direction);
+
+        float inverse_square_dropoff = invert_direction * invert_direction;
+
+        acceleration.x = normalize_direction.x * planet.gravity_strengh * inverse_square_dropoff;
+        acceleration.y = normalize_direction.y * planet.gravity_strengh * inverse_square_dropoff;
+
+        velocity.x += acceleration.x; // * dt;
+        velocity.y += acceleration.y; // * dt;
+
+        position.x += velocity.x; // * dt;
+        position.y += velocity.y; // * dt;
+        
     }
 
     void render(sf::RenderWindow &win){
+        circle.setPosition(position);
         win.draw(circle);
     }
 };
 
-float distance(sf::Vector2f &a, sf::Vector2f &b){
-    return sqrt((b.x - a.x)*(b.x - a.x) + (b.y - a.y)*(b.y - a.y));
-}
-
-sf::Vector2f force(Particle &a, Particle &b){
-    double gravity_constant = 6.67428e-11;
-
-    sf::Vector2f u = (b.position - a.position) / distance(a.position, b.position);
-
-    float dist = distance(a.position, b.position);
-
-    double scale_factor = (gravity_constant * a.mass * b.mass) / (1.6434e17);
-    sf::Vector2f Force(scale_factor * u.x, scale_factor * u.y);
-
-    float ax = Force.x / (a.mass * b.mass);
-    float ay = Force.y / (a.mass * b.mass);
-
-    return sf::Vector2f(ax, ay); // acceleration
-}
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1280, 720), "Moon Simulation");
+    window.setFramerateLimit(60);
 
     sf::Clock clock;
-    float dt;  
+    float dt;
 
-    Particle moon(20.0, sf::Vector2f(400, 360), sf::Vector2f(1, 1), 7.35e22);
-    Particle planet(40.0, sf::Vector2f(620, 360), sf::Vector2f(0, 0), 5.97e24);
+    Planet planet(sf::Vector2f(620, 360), 7000, 30);
+
+    Moon moon(sf::Vector2f(620, 600), sf::Vector2f(5, 0), 10);
 
     while(window.isOpen())
     {
         sf::Event event;
         while(window.pollEvent(event)){
-            if(event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
+            if(event.type == sf::Event::Closed) window.close();
+
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) window.close();
         }
 
         dt = clock.restart().asSeconds();
 
-        sf::Vector2f acceleration = force(planet, moon);
-
-        cout << acceleration.x << " " << acceleration.y << endl;
-
-        moon.acceleration = acceleration;
-
         window.clear();
 
-        moon.update(dt);
+        moon.update(planet, dt);
+
         moon.render(window);
-        planet.update(dt);
+        
         planet.render(window);
+
         window.display();
 
     }
